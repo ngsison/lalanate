57cd0d7d86bcb15e3c164514686ae6fe066149b1
Nathaniel Brion Sison <nathaniel.sison@mynt.xyz>
2020-12-11 00:03:06 +0800

    Add skeleton layout for DeliveryCell

---
+++ lalanate/Views/Cells/DeliveryCell.swift
@@ -0,0 +1,117 @@
+//
+//  DeliveryCell.swift
+//  lalanate
+//
+//  Created by Nathaniel Brion Sison on 12/10/20.
+//
+
+import UIKit
+import SnapKit
+
+class DeliveryCell: UITableViewCell {
+  
+  static let reuseIdentifier = "DeliveryCell"
+  static let preferredHeight: CGFloat = 120
+  
+  // MARK: - UI Props
+  
+  private lazy var containerView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .lightGray
+    view.layer.cornerRadius = 10
+    view.clipsToBounds = true
+    return view
+  }()
+  
+  private lazy var goodsImageView: UIImageView = {
+    let iv = UIImageView()
+    iv.contentMode = .scaleAspectFill
+    iv.clipsToBounds = true
+    iv.backgroundColor = .orange
+    return iv
+  }()
+  
+  private lazy var fromLabel: UILabel = {
+    let label = UILabel()
+    label.font = .systemFont(ofSize: 14)
+    label.text = "From: Nathaniel Brion Sison"
+    return label
+  }()
+  
+  private lazy var toLabel: UILabel = {
+    let label = UILabel()
+    label.font = .systemFont(ofSize: 14)
+    label.text = "To: Steve Jobs"
+    label.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+    return label
+  }()
+  
+  private lazy var favoriteImageView: UIImageView = {
+    let iv = UIImageView()
+    iv.contentMode = .scaleAspectFit
+    iv.backgroundColor = .red
+    return iv
+  }()
+  
+  private lazy var deliveryFeeLabel: UILabel = {
+    let label = UILabel()
+    label.font = .systemFont(ofSize: 14)
+    label.text = "$85"
+    label.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+    return label
+  }()
+  
+  // MARK: - Lifecycle Events
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    setUpViews()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+  
+  // MARK: - Private Methods
+  
+  public func setUpViews() {
+    
+    containerView.addSubview(goodsImageView)
+    goodsImageView.snp.makeConstraints { (make) in
+      make.top.left.bottom.equalToSuperview()
+      make.width.equalTo(120)
+    }
+    
+    containerView.addSubview(fromLabel)
+    fromLabel.snp.makeConstraints { (make) in
+      make.bottom.equalTo(containerView.snp.centerY).offset(-4)
+      make.left.equalTo(goodsImageView.snp.right).offset(10)
+    }
+    
+    containerView.addSubview(toLabel)
+    toLabel.snp.makeConstraints { (make) in
+      make.top.equalTo(containerView.snp.centerY).offset(4)
+      make.left.equalTo(fromLabel)
+    }
+    
+    containerView.addSubview(favoriteImageView)
+    favoriteImageView.snp.makeConstraints { (make) in
+      make.centerY.equalTo(fromLabel.snp.centerY)
+      make.right.equalToSuperview().offset(-10)
+      make.left.greaterThanOrEqualTo(fromLabel.snp.right).offset(10)
+      make.width.height.equalTo(20)
+    }
+    
+    containerView.addSubview(deliveryFeeLabel)
+    deliveryFeeLabel.snp.makeConstraints { (make) in
+      make.centerY.equalTo(toLabel)
+      make.left.greaterThanOrEqualTo(toLabel.snp.right).offset(10)
+      make.right.equalToSuperview().offset(-10)
+    }
+    
+    addSubview(containerView)
+    containerView.snp.makeConstraints { (make) in
+      make.edges.equalToSuperview().inset(UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+    }
+  }
+}
--- lalanate/Views/DeliveriesVC.swift
+++ lalanate/Views/DeliveriesVC.swift
@@ -10,14 +10,30 @@ import SnapKit
 
 class DeliveriesVC: UIViewController {
   
+  // MARK: - UI Props
+  
   private lazy var tableView: UITableView = {
     let tv = UITableView()
+    tv.separatorStyle = .none
     return tv
   }()
+  
+  // MARK: - Lifecycle Events
   
   override func viewDidLoad() {
     super.viewDidLoad()
     setUpViews()
+    setUpTableView()
+  }
+  
+  // MARK: - Private Methods
+  
+  private func setUpTableView() {
+    
+    tableView.dataSource = self
+    tableView.delegate = self
+    
+    tableView.register(DeliveryCell.self, forCellReuseIdentifier: DeliveryCell.reuseIdentifier)
   }
   
   private func setUpViews() {
@@ -28,5 +44,38 @@ class DeliveriesVC: UIViewController {
     tableView.snp.makeConstraints { (make) in
       make.edges.equalToSuperview()
     }
+  }
+}
+
+// MARK: - UITableViewDataSource
+
+extension DeliveriesVC: UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    return 5
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: DeliveryCell.reuseIdentifier,
+                                             for: indexPath) as! DeliveryCell
+    
+    return cell
+  }
+}
+
+// MARK: - UITableViewDelegate
+
+extension DeliveriesVC: UITableViewDelegate {
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    
+    return DeliveryCell.preferredHeight
   }
 }
